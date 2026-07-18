4eeb81dbfc677bfc16c3908d92506b9bcca6df4a
littinrajan <littinrajan@gmail.com>
2026-07-18 05:56:51 +0530

    feat(core): add execution event types

--- flowdebug/core/__init__.py
+++ flowdebug/core/__init__.py
@@ -1,8 +1,5 @@
 """
 Core execution models used throughout FlowDebug.
-
-This package contains the foundational types shared by the tracer,
-recorder, storage layer, reporters, and plugins.
 """
 
 from .enums import EventType
--- flowdebug/core/enums.py
+++ flowdebug/core/enums.py
@@ -6,10 +6,33 @@ from enum import Enum
 
 
 class EventType(str, Enum):
-    """
-    Types of execution events captured by FlowDebug.
-    """
+    """Supported execution event types."""
 
     CALL = "call"
     RETURN = "return"
     EXCEPTION = "exception"
+
+    LINE = "line"
+
+    VARIABLE_CREATED = "variable_created"
+    VARIABLE_UPDATED = "variable_updated"
+    VARIABLE_DELETED = "variable_deleted"
+
+    IMPORT = "import"
+
+    THREAD_START = "thread_start"
+    THREAD_END = "thread_end"
+
+    TASK_START = "task_start"
+    TASK_END = "task_end"
+
+    FILE_OPEN = "file_open"
+    FILE_CLOSE = "file_close"
+
+    HTTP_REQUEST = "http_request"
+    HTTP_RESPONSE = "http_response"
+
+    SQL_QUERY = "sql_query"
+
+    LOG = "log"
+
